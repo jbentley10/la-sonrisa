ab3daed3d4b8df733a0cffdef76a07bb8c2bae6d
John Bentley <jwbentle@gmail.com>
2023-10-09 16:01:43 -0700

    Add new images

--- app/components/hero.js
+++ app/components/hero.js
@@ -6,21 +6,15 @@ const openSans = Open_Sans({ subsets: ['latin'], weight: ['300'], })
 
 export default function Hero() {
     return (
-        <div id="hero" className="h-5/6 py-20">
+        <div id="hero" className="h-5/6 pb-20">
             <div className={rye.className}>
                 <div id="logo-container" className="flex justify-center items-cente pb-8">
                     <Image
-                        src="/logo.png"
-                        width={150}
-                        height={150}
+                        src="/logo-pink.webp"
+                        width={500}
+                        height={500}
                         alt="La Sonrisa skull logo"
                     />
-                </div>
-                <h1 id="hero-heading" className="text-center text-6xl">La Sonrisa</h1>
-                <div className={openSans.className}>
-                    <h2 id="hero-subheading" className="text-center text-4xl uppercase pt-4">Photo Booth</h2>
-                    <h3 id="hero-subheading-2" className="text-center text-2xl uppercase pt-4">OC - LA - SD</h3>
-                    {/* <Image src="/logo.png" /> */}
                 </div>
             </div>
         </div>
